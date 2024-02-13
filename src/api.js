5a883c25cf305956cc35c7e10a0fcfc87de04250
kensanvictores07 <kensanvictores07@gmail.com>
2024-02-13 21:31:51 +0800

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5`;
+export const WEATHER_API_KEY = `5b0494cffc81de4048a87a18c1f5c0db`
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '46e109d2d9mshd70e3fcd44ad1ddp10b4c3jsn47dc08dbd27a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
---
+++ src/component/search/Search.js
@@ -0,0 +1,41 @@
+import { AsyncPaginate } from "react-select-async-paginate";
+import { useState } from "react";
+import { geoApiOptions, GEO_API_URL } from "../../api";
+
+export default function Search({ onSearchChange }) {
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        return {
+          options: res.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+}
